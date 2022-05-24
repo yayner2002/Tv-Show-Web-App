570cb6dff317c1dded0ad3f9ecab950ff25db9c4
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-24 12:26:57 +0200

    Modify the files for  linters

--- src/modules/showMovies.js
+++ src/modules/showMovies.js
@@ -1,51 +1,51 @@
 const movies = [
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
-    {
-        image: "/images/movie1.jpg",
-        ratings: 3,
-        title: 'Hard Target',
-    },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
+  {
+    image: '/images/movie1.jpg',
+    ratings: 3,
+    title: 'Hard Target',
+  },
 ];
 
 const movieDisplay = document.querySelector('.movies-display');
 const showMovies = () => {
-    movieDisplay.innerHTML = "";
-    movies.forEach(movie => {
-        let movieTag = `
+  movieDisplay.innerHTML = '';
+  movies.forEach((movie) => {
+    const movieTag = `
         <div class="movie-content">
         <div class="card-image">
         <img src="${movie.image}" alt="movies">
@@ -60,8 +60,8 @@ const showMovies = () => {
         <button class="movie-reservation" id="reservation-btn">Reservations</button>
         </div>
         </div>`;
-        movieDisplay.innerHTML += movieTag;
-    });
-    return movieDisplay;
-}
+    movieDisplay.innerHTML += movieTag;
+  });
+  return movieDisplay;
+};
 export default showMovies;
