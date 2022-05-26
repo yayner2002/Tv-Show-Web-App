f76a1e4b6e7b9c100342f9544b13d2cce9c05da3
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-26 17:38:28 +0200

    Resolve conflicts

--- src/index.js
+++ src/index.js
@@ -9,13 +9,10 @@ document.addEventListener('DOMContentLoaded', showMovies);
 
 const displayItem = async () => {
   const movies = await getData();
-<<<<<<< HEAD
   const total = movies.slice(0, 40);
   showMovies(total);
   commentsListner();
-=======
   showMovies(movies);
   reservationListener();
->>>>>>> 43e7becfcedfa1fd76c55e2673065f78cbca3f3d
 };
 displayItem();
