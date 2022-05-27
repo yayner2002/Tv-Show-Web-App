de8a6b39e477f842bcc9ef8c81deccad66499fd8
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-27 21:31:04 +0200

    Change the directory to dist

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
   },
   //   optimization: {
   //     runtimeChunk: 'single',
